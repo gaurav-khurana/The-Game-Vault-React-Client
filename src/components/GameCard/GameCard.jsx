687b6b82d587045af07c5bb6c86c26beef9d7b71
Gaurav <program.drgk@gmail.com>
2024-04-08 00:16:26 -0700

    Game card Dynamic routing to xbox or PS page based on URL DONE

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,28 +1,43 @@
 import "./GameCard.scss";
-// import GodOfWar from "../../assets/images/GOW1.webp";
 // import Spiderman from "../../assets/images/spiderman1.png";
-// import Spiderman2 from "../../assets/images/spiderman2.jpeg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function GameCard({ game }) {
+  // const { console } = useParams;
+
+  const navigate = useNavigate();
+
+  const id = game.id;
+
+  const handleConsole = () => {
+    if (id >= 1 && id <= 10) {
+      navigate(`/games/xbox/${game.id}`);
+      return;
+    }
+    if (id >= 11 && id <= 20) {
+      navigate(`/games/ps/${game.id}`);
+      return;
+    }
+  };
+
   return (
     <>
       {/* <div>GameCard</div> */}
-      <article className="gamecard">
+      <article className="gamecard" onClick={handleConsole}>
         {/* <Link to={`/games/ps/${game.id}`}> */}
-        <Link to={`/games/xbox/${game.id}`}>
-          <img
-            className="gamecard__image"
-            src={game.image1}
-            //   src={GodOfWar}
-            //   src={Spiderman}
-            //   src={Spiderman2}
-            alt="God Of War Ragnarok"
-          />
-          <h3 className="gamecard__title">{game.title}</h3>
-          {/* <h3 className="gamecard__title">God of War: Ragnarok</h3> */}
-          {/* <h3 className="gamecard__title">Spiderman 2 Miles Morales</h3> */}
-        </Link>
+        {/* <Link to={`/games/xbox/${game.id}`}> */}
+        {/* <Link to={`/games/${console}/${game.id}`}> */}
+        <img
+          className="gamecard__image"
+          src={game.image1}
+          //   src={Spiderman}
+          alt={`Image of ${game.title}`}
+        />
+
+        <h3 className="gamecard__title">{game.title}</h3>
+
+        {/* <h3 className="gamecard__title">Spiderman 2 Miles Morales</h3> */}
+        {/* </Link> */}
       </article>
     </>
   );
