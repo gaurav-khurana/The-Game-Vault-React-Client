cb101392d9acac75b66192cad8b6b58a24fc7646
Gaurav <program.drgk@gmail.com>
2024-04-11 12:09:02 -0700

    Order confirmation Page - Mobile & Desktop DONE

--- src/pages/OrderConfirmedPage/OrderConfirmedPage.jsx
+++ src/pages/OrderConfirmedPage/OrderConfirmedPage.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
 import "./OrderConfirmedPage.scss";
 
 function OrderConfirmedPage() {
   return (
     <>
-      <div>OrderConfirmedPage</div>
+      <main className="confirmation">
+        <section className="xbox-game-header">
+          <Link to="/">
+            <h2 className="xbox-game-header__logo">[G]</h2>
+          </Link>
+        </section>
+
+        <div className="confirmation-container">
+          <h2 className="confirmation__title">Order placed successfully !</h2>
+
+          <h2 className="confirmation__text">
+            Your order details and receipt will be sent to you via email
+          </h2>
+
+          <Link to="/menu">
+            <button className="confirmation__btn" type="submit">
+              Continue Shopping
+            </button>
+          </Link>
+        </div>
+      </main>
     </>
   );
 }
